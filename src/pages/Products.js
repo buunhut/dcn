5b51061539d368e80058955b2556aeb2feacd9a7
Buu Nhut <nhut.nta@gmail.com>
2024-07-02 16:24:49 +0700

    sp

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -3,11 +3,76 @@ import { useParams } from "react-router-dom";
 import Header from "../component/Header";
 import "./products.scss";
 import Footer from "../component/Footer";
+import Caterogy from "../component/Caterogy";
 
 const Products = () => {
   let { id } = useParams();
 
-  return <div className="content">Show products of {id}</div>;
+  const listProducts = [
+    {
+      caterogy: "cloud-platform",
+      productName: "Bộ phát wifi",
+      price: 5000000,
+      img: "sp1.jpeg",
+      detail: "mô tả chi tiết NW5678",
+    },
+    {
+      caterogy: "cloud-platform",
+      productName: "Thiết bị chuyển mạch hiệu DCN NW9978",
+      price: 7000000,
+      img: "",
+      detail: "mô tả chi tiết NW9978",
+    },
+    {
+      caterogy: "cloud-platform",
+      productName: "Thiết bị chuyển mạch hiệu DCN NW5678",
+      price: 5000000,
+      img: "",
+      detail: "mô tả chi tiết NW5678",
+    },
+    {
+      caterogy: "cloud-platform",
+      productName: "Thiết bị chuyển mạch hiệu DCN NW9978",
+      price: 9000000,
+      img: "",
+      detail: "mô tả chi tiết NW9978",
+    },
+    {
+      caterogy: "cloud-platform",
+      productName: "Thiết bị chuyển mạch hiệu DCN NW5678",
+      price: 5000000,
+      img: "",
+      detail: "mô tả chi tiết NW5678",
+    },
+    {
+      caterogy: "cloud-platform",
+      productName: "SWITCH KHÔNG QUẢN LÝ 8 CỔNG 10/100M DCN ES1008D",
+      price: 324000,
+      img: "sp2.jpeg",
+      detail: "mô tả chi tiết NW9978",
+    },
+  ];
+
+  return (
+    <div className="content">
+      {/* Show products of {id} */}
+      <div className="productContent">
+        {listProducts?.map((item, index) => {
+          const { productName, price, img, detail } = item;
+          console.log(img);
+          return (
+            <div key={index} className="productItem">
+              <div className="img">
+                <img src={`/img/${img}`} alt="" />
+              </div>
+              <h3>{productName.toUpperCase()}</h3>
+              <p>{price.toLocaleString()}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Products;
