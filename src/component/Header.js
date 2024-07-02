772eaca5244bb3351a3e9727c045c00f4ca6fca9
Buu Nhut <nhut.nta@gmail.com>
2024-07-02 20:44:33 +0700

    menu

--- src/component/Header.js
+++ src/component/Header.js
@@ -171,155 +171,60 @@ const Header = () => {
           {/* <h1>DCN</h1> */}
           {/* <p>Vietnam</p> */}
           <div className="menu" onClick={() => setSmallMenu(!smallMenu)}>
-            Menu
+            <i className="fa-solid fa-bars"></i>
           </div>
           <img src="./img/logo.png" alt="" onClick={handleHomePage} />
         </div>
 
         <div className="container" style={{ width: smallMenu ? "100%" : 0 }}>
-          <div className="menu">
-            <ul className="menuContent">
-              <li className="menuItem">
-                <NavLink to="/products">Products</NavLink>
-                {/* <i className="fa-solid fa-angle-down"></i> */}
-                {/* <div className="dropDown">
-                <ul className="ulDropDown">
-                  <li className="liDropDown">
-                    <NavLink to="/products/cloud-platform">
-                      Cloud Platform
-                    </NavLink>
-                  </li>
-                  <li className="liDropDown">
-                    <NavLink to="/products/ethernet-switch">
-                      Ethernet Switch
-                    </NavLink>
-                  </li>
-                  <li className="liDropDown">
-                    <NavLink to="/products/enterprise-wi-fi">
-                      Enterprise Wi-Fi
-                    </NavLink>
-                  </li>
-                  <li className="liDropDown">
-                    <NavLink to="/products/multi-core-edge">
-                      Multi-core Edge
-                    </NavLink>
-                  </li>
-                  <li className="liDropDown">
-                    <NavLink to="/products/firewall">Firewall</NavLink>
-                  </li>
-                  <li className="liDropDown">
-                    <NavLink to="/products/data-storage">Data Storage</NavLink>
-                  </li>
-                  <li className="liDropDown">
-                    <NavLink to="/products/eco-products">Eco-products</NavLink>
-                  </li>
-                  <li className="liDropDown">
-                    <NavLink to="/products/server">Server</NavLink>
-                  </li>
-                </ul>
-              </div> */}
-              </li>
-              <li className="menuItem">
-                <NavLink to="solutions">Solutions</NavLink>
-                {/* <i className="fa-solid fa-angle-down"></i> */}
-                {/* <div className="dropDown">
-                <ul className="ulDropDown">
-                  <li className="liDropDown">
-                    <a href="">Campus Solution</a>
-                  </li>
-                  <li className="liDropDown">
-                    <a href="">Application Solution</a>
-                  </li>
-                  <li className="liDropDown">
-                    <a href="">ISP Solution</a>
-                  </li>
-                  <li className="liDropDown">
-                    <a href="">Data Center Solution</a>
-                  </li>
-                  <li className="liDropDown">
-                    <a href="">Retail Solution</a>
-                  </li>
-                  <li className="liDropDown">
-                    <a href="">Warehouse Solution</a>
-                  </li>
-                  <li className="liDropDown">
-                    <a href="">Case Study</a>
-                  </li>
-                </ul>
-              </div> */}
-              </li>
-              {/* <li className="menuItem">
-              Support <i className="fa-solid fa-angle-down"></i>
-              <div className="dropDown">
-                <ul className="ulDropDown">
-                  <li className="liDropDown">
-                    <a href="">Downloads</a>
-                  </li>
-                  <li className="liDropDown">
-                    <a href="">DCN Training</a>
-                  </li>
-                  <li className="liDropDown">
-                    <a href="">Service Policy</a>
-                  </li>
-                  <li className="liDropDown">
-                    <a href="">Seri Check</a>
-                  </li>
-                </ul>
-              </div>
-            </li> */}
-              <li className="menuItem">
-                <NavLink to="/partners">Partners</NavLink>
-                {/* <i className="fa-solid fa-angle-down"></i> */}
-                {/* <div className="dropDown">
-                <ul className="ulDropDown">
-                  <li className="liDropDown">
-                    <a href="">DCNet</a>
-                  </li>
-                  <li className="liDropDown">
-                    <a href="">Vendor</a>
-                  </li>
-                </ul>
-              </div> */}
-              </li>
-              <li className="menuItem">
-                <NavLink to="/contact-us">Contact Us</NavLink>
-                {/* <i className="fa-solid fa-angle-down"></i> */}
-                {/* <div className="dropDown">
-                <ul className="ulDropDown">
-                  <li className="liDropDown">
-                    <a href="">DCN Brief</a>
-                  </li>
-                  <li className="liDropDown">
-                    <a href="">Parent Company</a>
-                  </li>
-                  <li className="liDropDown">
-                    <a href="">News</a>
-                  </li>
-                  <li className="liDropDown">
-                    <a href="">Contact Us</a>
-                  </li>
-                </ul>
-              </div> */}
-              </li>
-            </ul>
-          </div>
           <div className="menuExtra">
+            <div className="langues">
+              English <i className="fa-solid fa-angle-down"></i>
+            </div>
             <div className="search">
               <input type="text" placeholder="Search..." />
               <i className="fa-solid fa-magnifying-glass"></i>
             </div>
-            <div className="langues">
-              English <i className="fa-solid fa-angle-down"></i>
-            </div>
-            {/* <div className="login">
-            <div className="icon" onClick={handleDangNhap}>
-              <i class="fa-solid fa-user"></i>
-            </div>
-            Chưa đăng nhập
-          </div> */}
+          </div>
+
+          <div className="menu">
+            <ul className="menuContent">
+              <li
+                className="menuItem"
+                onClick={() => {
+                  setSmallMenu(false);
+                }}
+              >
+                <NavLink to="/products">Products</NavLink>
+              </li>
+              <li
+                className="menuItem"
+                onClick={() => {
+                  setSmallMenu(false);
+                }}
+              >
+                <NavLink to="solutions">Solutions</NavLink>
+              </li>
+              <li
+                className="menuItem"
+                onClick={() => {
+                  setSmallMenu(false);
+                }}
+              >
+                <NavLink to="/partners">Partners</NavLink>
+              </li>
+              <li
+                className="menuItem"
+                onClick={() => {
+                  setSmallMenu(false);
+                }}
+              >
+                <NavLink to="/contact-us">Contact Us</NavLink>
+              </li>
+            </ul>
           </div>
           <div className="close" onClick={() => setSmallMenu(!smallMenu)}>
-            Đóng
+            <i className="fa-solid fa-xmark"></i>
           </div>
         </div>
       </div>
