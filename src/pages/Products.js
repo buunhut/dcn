eb09a0001ed83097500d0e930bddd5f4c519258c
Buu Nhut <nhut.nta@gmail.com>
2024-07-03 10:29:20 +0700

    redux

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,58 +1,9 @@
 import React from "react";
-import { useParams } from "react-router-dom";
-import Header from "../component/Header";
 import "./products.scss";
-import Footer from "../component/Footer";
-import Caterogy from "../component/Caterogy";
+import { useSelector } from "react-redux";
 
 const Products = () => {
-  let { id } = useParams();
-
-  const listProducts = [
-    {
-      caterogy: "cloud-platform",
-      productName: "Bộ phát wifi",
-      price: 5000000,
-      img: "sp1.jpeg",
-      detail: "mô tả chi tiết NW5678",
-    },
-    {
-      caterogy: "cloud-platform",
-      productName: "Thiết bị chuyển mạch hiệu DCN NW9978",
-      price: 7000000,
-      img: "",
-      detail: "mô tả chi tiết NW9978",
-    },
-    {
-      caterogy: "cloud-platform",
-      productName: "Thiết bị chuyển mạch hiệu DCN NW5678",
-      price: 5000000,
-      img: "",
-      detail: "mô tả chi tiết NW5678",
-    },
-    {
-      caterogy: "cloud-platform",
-      productName: "Thiết bị chuyển mạch hiệu DCN NW9978",
-      price: 9000000,
-      img: "",
-      detail: "mô tả chi tiết NW9978",
-    },
-    {
-      caterogy: "cloud-platform",
-      productName: "Thiết bị chuyển mạch hiệu DCN NW5678",
-      price: 5000000,
-      img: "",
-      detail: "mô tả chi tiết NW5678",
-    },
-    {
-      caterogy: "cloud-platform",
-      productName: "SWITCH KHÔNG QUẢN LÝ 8 CỔNG 10/100M DCN ES1008D",
-      price: 324000,
-      img: "sp2.jpeg",
-      detail: "mô tả chi tiết NW9978",
-    },
-  ];
-
+  const { listProducts } = useSelector((state) => state.dataSlice);
   return (
     <div className="content">
       {listProducts?.map((item, index) => {
---
+++ src/redux/dataSlice.js
@@ -0,0 +1,67 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  listProducts: [
+    {
+      productId: 1,
+      caterogy: "cloud-platform",
+      productName: "Bộ phát wifi",
+      price: 5000000,
+      img: "sp1.jpeg",
+      detail: "mô tả chi tiết NW5678",
+    },
+    {
+      productId: 2,
+      caterogy: "cloud-platform",
+      productName: "Thiết bị chuyển mạch hiệu DCN NW9978",
+      price: 7000000,
+      img: "",
+      detail: "mô tả chi tiết NW9978",
+    },
+    {
+      productId: 3,
+      caterogy: "cloud-platform",
+      productName: "Thiết bị chuyển mạch hiệu DCN NW5678",
+      price: 5000000,
+      img: "",
+      detail: "mô tả chi tiết NW5678",
+    },
+    {
+      productId: 4,
+      caterogy: "cloud-platform",
+      productName: "Thiết bị chuyển mạch hiệu DCN NW9978",
+      price: 9000000,
+      img: "",
+      detail: "mô tả chi tiết NW9978",
+    },
+    {
+      productId: 5,
+      caterogy: "cloud-platform",
+      productName: "Thiết bị chuyển mạch hiệu DCN NW5678",
+      price: 5000000,
+      img: "",
+      detail: "mô tả chi tiết NW5678",
+    },
+    {
+      productId: 6,
+      caterogy: "cloud-platform",
+      productName: "SWITCH KHÔNG QUẢN LÝ 8 CỔNG 10/100M DCN ES1008D",
+      price: 324000,
+      img: "sp2.jpeg",
+      detail: "mô tả chi tiết NW9978",
+    },
+  ],
+};
+
+const dataSlice = createSlice({
+  name: "data",
+  initialState,
+  reducers: {
+    updateListProducts(state, action) {
+      state.listProducts = action.payload;
+    },
+  },
+});
+
+export const { updateListProducts } = dataSlice.actions;
+export default dataSlice.reducer;
